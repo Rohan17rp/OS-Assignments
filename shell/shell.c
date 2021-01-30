180d94d11593abd76ba9c12c8abce9f7128ffb5d
Rohan <rohan17.rp@gmail.com>
2021-01-30 23:08:50 +0530

    Written functions for string handling

--- shell/shell.c
+++ shell/shell.c
@@ -8,43 +8,26 @@
 #define MAX_ARGS 5 
 #define SIZE 128
 
+void get_command(char*);
+int tokenize_command(char**, char*);
+void print_args(char** arg, int arg_count);
 
 int main(){
-	char ch;
 	char *arg[MAX_ARGS];
 	char cmd[SIZE];
-	char *token;
 	int pid;
 	while(1){
-		int arg_count = 0, count = 0, i;
-		printf("Potato Prompt-->");
-
-		while(1){
-			ch = getchar();
-			if(ch != '\n'){
-				cmd[count] = ch;
-				count++;
-			}
-			else{
-				cmd[count] = '\0';
-				break;
-			}
-		}
+		int arg_count = 0, i;
+		printf("potato-prompt-->");
+	
+		// Get user input	
+		get_command(cmd);
 		
-		i = 0;
-		token = strtok(cmd, " ");
-		while(token != NULL){
-			arg[i] = token;
-			i++;
-			token = strtok(NULL, " ");
-		}
-		arg[i] = NULL;
+		arg_count = tokenize_command(arg, cmd);	
 
 		// Printing arguments for debugging
-		for(i = 0; i < arg_count; i++){
-			printf("%s\n", arg[i]);
-		}
-		
+		// print_args(arg, arg_count);
+
 		// Make copy of process
 		pid = fork();
 		
@@ -58,5 +41,48 @@ int main(){
 		else{
 			wait(0);
 		}
+	}
+}
+
+/*
+ * Get input from user
+ * */
+void get_command(char* cmd){
+	char ch;
+	int count = 0;
+	while(1){
+		ch = getchar();
+		if(ch != '\n'){
+			cmd[count] = ch;
+			count++;
+		}
+		else{
+			cmd[count] = '\0';
+			break;
+		}
+	}
+}
+
+/*
+ * Breaks string by spaces and saves strings in new array
+ * Returns number of strings formed
+ * */
+int tokenize_command(char** arg, char* cmd){
+	char *token;
+	int i = 0;
+	token = strtok(cmd, " ");
+	while(token != NULL){
+		arg[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+	arg[i] = NULL;
+	return i;
+}
+
+void print_args(char** arg, int arg_count){
+	int i =0;
+	for(i = 0; i < arg_count; i++){
+		printf("%s\n", arg[i]);
 	}
 }
